6a5f6fdd960a1fc536ca5f197d00852b92ad3a11
Alejandra <146900779+AlejandraMedranda0906@users.noreply.github.com>
2023-12-21 20:18:05 -0500

    2

---
+++ prueba.js
@@ -0,0 +1,14 @@
+//A a partir del array ages=[15, 45, 50] agregue su edad.
+// Usando un for con un if, muestre en pantalla  solo las edades mayores a 25
+
+'use strict'
+
+let ages = [15, 45, 50,];
+
+for (let i = 0; i < ages.length; i++) {
+  if (ages[i] > 25) {
+    alert(ages[i]);
+
+  }
+}
+
